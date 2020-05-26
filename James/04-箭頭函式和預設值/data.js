3dea44211977dc235e95515e362ed0cd7606a696
Ader <Ader@gmail.com>
2020-05-26 10:02:28 +0800

    ADD

---
+++ James/04-箭頭函式和預設值/data.js
@@ -0,0 +1,50 @@
+var data = [
+    {
+        idx: 1,
+        name: "小豬",
+        age: 12,
+        sex: "male"
+    },
+    {
+        idx: 2,
+        name: "小烏龜",
+        age: 20,
+        sex: "female"
+    },
+    {
+        idx: 3,
+        name: "小鳥",
+        age: 16,
+        sex: "male"
+    },
+    {
+        idx: 4,
+        name: "小貓",
+        age: 35,
+        sex: "male"
+    },
+    {
+        idx: 5,
+        name: "小狗",
+        age: 19,
+        sex: "male"
+    },
+    {
+        idx: 6,
+        name: "鱷魚",
+        age: 40,
+        sex: "female"
+    },
+    {
+        idx: 7,
+        name: "龍蝦",
+        age: 2,
+        sex: "female"
+    },
+    {
+        idx: 8,
+        name: "海藻",
+        age: 30,
+        sex: "male"
+    }
+]
